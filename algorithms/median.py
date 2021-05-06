185f0b65dfe349174b10af6bce5ab20578405e4a
Djiwan17 <82372265+Djiwan17@users.noreply.github.com>
2021-05-06 23:07:26 +0500

    Create median.py

---
+++ algorithms/median.py
@@ -0,0 +1,20 @@
+def median_of_medians(A, i):
+
+    sublists = [A[j:j+5] for j in range(0, len(A), 5)]
+    medians = [sorted(sublist)[len(sublist)/2] for sublist in sublists]
+    if len(medians) <= 5:
+        pivot = sorted(medians)[len(medians)/2]
+    else:
+        pivot = median_of_medians(medians, len(medians)/2)
+
+    
+    low = [j for j in A if j < pivot]
+    high = [j for j in A if j > pivot]
+
+    k = len(low)
+    if i < k:
+        return median_of_medians(low,i)
+    elif i > k:
+        return median_of_medians(high,i-k-1)
+    else:
+        return pivot
